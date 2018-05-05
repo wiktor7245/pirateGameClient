d841572804dee95e2fc67c98858914647f2f08a4
wiktor7245 <wiktor7245@o2.pl>
2018-05-05 12:47:17 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var y = document.getElementById("XD").value;
+    //document.getElementById("gen").innerHTML = y + ":5000";
+    window.location.href = "http://" + y + ":5000";
+}
